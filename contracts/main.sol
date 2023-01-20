67b9e2780e5d4e8ade79103df15b86c0bbafd248
m-principato <marc@principato.com.de>
2023-01-20 01:27:41 +0100

    Stable Main Commit

--- contracts/main.sol
+++ contracts/main.sol
@@ -16,6 +16,8 @@ contract Main is ERC1155, IERC721Receiver, Pausable, Ownable, ERC1155Burnable, E
     using SafeMath for uint256; //using SafeMath Library to avoid integer overflow-/underflow attacks
     using Counters for Counters.Counter;
 
+    Counters.Counter private _intIDcounter;
+
 //Custom Fractionalizer declarations
     struct infoStorage {
             DepositInfo[] Deposit;
@@ -24,10 +26,10 @@ contract Main is ERC1155, IERC721Receiver, Pausable, Ownable, ERC1155Burnable, E
     struct DepositInfo {
         address owner;
         
-        address extAddress;
-        uint256 extID;
+        address Ext_NFT_Address;
+        uint256 Ext_NFT_ID;
 
-        uint256 intID;
+        uint256 Int_NFT_ID;
         uint256 depositTimestamp;
         
         uint256 totalCO2O;
@@ -45,18 +47,18 @@ contract Main is ERC1155, IERC721Receiver, Pausable, Ownable, ERC1155Burnable, E
 
 //Custom modifiers:
 
-    modifier fractionalized0(uint256 _NFTindex) {
-        require(UserToDeposits[msg.sender].Deposit[_NFTindex].fractionalized == false, "Token has not been fractionalized yet");
+    modifier fractionalized0(uint256 _Int_NFT_ID) {
+        require(UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractionalized == false, "Token has not been fractionalized yet");
         _;
     }   
 
-    modifier fractionalized1(uint256 _NFTindex) {
-        require(UserToDeposits[msg.sender].Deposit[_NFTindex].fractionalized == true, "Token already has been fractionalized");
+    modifier fractionalized1(uint256 _Int_NFT_ID) {
+        require(UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractionalized == true, "Token already has been fractionalized");
         _;
     }  
 
-    modifier NFTowner(uint256 _NFTindex) {
-        require(UserToDeposits[msg.sender].Deposit[_NFTindex].owner == msg.sender, "Only the owner of this NFT can access it");
+    modifier NFTowner(uint256 _Int_NFT_ID) {
+        require(UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].owner == msg.sender, "Only the owner of this NFT can access it");
         _;
     }
 
@@ -86,61 +88,65 @@ contract Main is ERC1155, IERC721Receiver, Pausable, Ownable, ERC1155Burnable, E
     }
 
     //Custom fractionalizer functionalities:
-    function DepositNFT(address NFTaddress, uint256 extID, uint256 CO2O) external {
+    function DepositNFT(address NFTaddress, uint256 Ext_NFT_ID, uint256 CO2O) external {
       
-        ERC721(NFTaddress).safeTransferFrom(msg.sender, address(this), extID);
+        ERC721(NFTaddress).safeTransferFrom(msg.sender, address(this), Ext_NFT_ID);
 
         DepositInfo memory newDeposit;
+
             newDeposit.owner = msg.sender;
-            newDeposit.extAddress = NFTaddress;
-            newDeposit.extID = extID;
+            newDeposit.Ext_NFT_Address = NFTaddress;
+            newDeposit.Int_NFT_ID = _intIDcounter.current();
+            newDeposit.Ext_NFT_ID = Ext_NFT_ID;
             newDeposit.depositTimestamp = block.timestamp;
             newDeposit.totalCO2O = CO2O;
             newDeposit.fractions = 0;
             newDeposit.fractionalized = false;
         
-        NftIndex[NFTaddress][extID] = UserToDeposits[msg.sender].Deposit.length;
+        NftIndex[NFTaddress][Ext_NFT_ID] = UserToDeposits[msg.sender].Deposit.length;
+
+        _intIDcounter.increment();
 
         UserToDeposits[msg.sender].Deposit.push(newDeposit);
 
     }
 
-    function getDepositInfo(address account, address extAddress, uint256 extID) external view returns (address, address, uint256, uint256, bool, uint256, uint256) {
+    function getDepositInfo(address _Account, address _Ext_NFT_Address, uint256 _Ext_NFT_ID) external view returns (address, address, uint256, uint256, uint256, uint256, bool, uint256) {
  
-        uint256 _NFTindex = NftIndex[extAddress][extID]; // Look up the deposit information using the NftIndex mapping
+        uint256 _NFTindex = NftIndex[_Ext_NFT_Address][_Ext_NFT_ID]; // Look up the deposit information using the NftIndex mapping
 
-        DepositInfo storage deposit = UserToDeposits[account].Deposit[_NFTindex]; // Get the deposit information using the UserToDeposits mapping and the deposit index
+        DepositInfo storage deposit = UserToDeposits[_Account].Deposit[_NFTindex]; // Get the deposit information using the UserToDeposits mapping and the deposit index
 
-        return (deposit.owner, deposit.extAddress, deposit.extID, deposit.depositTimestamp, deposit.fractionalized, deposit.totalCO2O, deposit.fractions); // Return the relevant information from the deposit
+        return (deposit.owner, deposit.Ext_NFT_Address, deposit.Ext_NFT_ID, deposit.Int_NFT_ID, deposit.depositTimestamp, deposit.totalCO2O, deposit.fractionalized, deposit.fractions); // Return the relevant information from the deposit
     }
 
-    function WithdrawNFT(uint256 _NFTindex) external fractionalized0(_NFTindex) NFTowner(_NFTindex) {
+    function WithdrawNFT(uint256 _Int_NFT_ID) external fractionalized0(_Int_NFT_ID) NFTowner(_Int_NFT_ID) {
         
-        address _NFTaddress = UserToDeposits[msg.sender].Deposit[_NFTindex].extAddress;
-        uint256 _nftID =  UserToDeposits[msg.sender].Deposit[_NFTindex].extID;
+        address _NFTaddress = UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].Ext_NFT_Address;
+        uint256 _nftID =  UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].Ext_NFT_ID;
 
-        delete UserToDeposits[msg.sender].Deposit[_NFTindex];
+        delete UserToDeposits[msg.sender].Deposit[_Int_NFT_ID];
 
         ERC721(_NFTaddress).safeTransferFrom(address(this), msg.sender, _nftID);
     }
 
-    function FractionalizeNFT(uint256 fractions, uint256 _NFTindex) external fractionalized1(_NFTindex) NFTowner(_NFTindex) {
+    function FractionalizeNFT(uint256 fractions, uint256 _Int_NFT_ID) external fractionalized1(_Int_NFT_ID) NFTowner(_Int_NFT_ID) {
        
-        UserToDeposits[msg.sender].Deposit[_NFTindex].fractionalized = true;
-        UserToDeposits[msg.sender].Deposit[_NFTindex].fractions = fractions;
+        UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractionalized = true;
+        UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractions = fractions;
 
-        _mint(address(msg.sender), _NFTindex, fractions, "");
+        _mint(address(msg.sender), _Int_NFT_ID, fractions, "");
     }
 
-    function UnifyFractions(uint256 _NFTindex) external fractionalized0(_NFTindex) {
+    function UnifyFractions(uint256 _Int_NFT_ID) external fractionalized0(_Int_NFT_ID) {
         
-        uint256 totalFractions = UserToDeposits[msg.sender].Deposit[_NFTindex].fractions;
-        require(balanceOf(msg.sender, _NFTindex) == totalFractions, "Insufficient fractions");
+        uint256 totalFractions = UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractions;
+        require(balanceOf(msg.sender, _Int_NFT_ID) == totalFractions, "Insufficient fractions");
        
-        UserToDeposits[msg.sender].Deposit[_NFTindex].fractionalized = false;
-        UserToDeposits[msg.sender].Deposit[_NFTindex].fractions = 0;
+        UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractionalized = false;
+        UserToDeposits[msg.sender].Deposit[_Int_NFT_ID].fractions = 0;
         
-        _burn(address(msg.sender), _NFTindex, totalFractions);
+        _burn(address(msg.sender), _Int_NFT_ID, totalFractions);
     }
 
     //Required override by solidity for safely receiving ERC721 tokens
